954465afe747147ceeb408da53bbe57960b3301c
tibi.aki.tivadar@gmail.com <tibi.aki.tivadar@gmail.com>
2020-12-03 23:26:36 +0000

    Application is done, only testing left

--- student_management_sys.py
+++ student_management_sys.py
@@ -13,11 +13,12 @@
 # ------------------------------------------------------------------------------
 
 
+import re
 import math
 import csv
 import tkinter as tk
 from tkinter import messagebox
-import re
+
 
 # ---------------------------- Get Students From CSV ---------------------------
 
@@ -107,6 +108,7 @@ ACTIVE_FG = "#4cc3f1"
 ROW_BG_LIGHT = "#2a2a2a"
 ROW_BG_DARK = "#333"
 SELECTED_BG = "#95EAC1"
+MONOSPACE = ("Courier", 9)
 
 LRG_FONT = 12
 MID_FONT = 10
@@ -756,7 +758,7 @@ def submitStudent(student_inputs, check_btn_flags, dialog, method, selected_inde
         return msg("Exactly 4 subjects must be selected!")
 
     def padZeros(num):
-        return "0" + num if int(num) < 10 else num
+        return "0" + str(int(num)) if int(num) < 10 else num
 
     new_student = {
         "f_name": f_name,
@@ -1072,9 +1074,16 @@ def renderNewDialog(method):
     # Because user can select other elements while submitting, consequently
     # changing the index of the student being under modification
 
+    def getSelected():
+        try:
+            return state["selected"][0]
+        except IndexError:
+            return None
+
     new_submit = DialogButton(
         new_body, text="Submit", width=10,
-        command=lambda: submitStudent(new_dialog_vars, check_vars, new_frame, method, state["selected"][0]))
+        command=lambda: submitStudent(
+            new_dialog_vars, check_vars, new_frame, method, getSelected()))
     new_submit.grid(row=15, column=0, columnspan=2)
 
     # New Dialog Separator 6
@@ -1132,6 +1141,34 @@ def renderNewDialog(method):
             new_subject_9_check.select()
 
 
+def renderDeleteMsg():
+    global students
+
+    if not len(state["selected"]):
+        tk.messagebox.showinfo(
+            "Delete",
+            "You need to select a student that you want to delete.\nYou can select a student by clicking the checkbox\nat the end of student details. ['\u25A3'] ['\u25A1']")
+        return
+
+    result = tk.messagebox.askquestion(
+        "Delete Warning",
+        f"You have selected {len(state['selected'])} item(s) to delete! Delete item(s)?",
+        icon='warning')
+    if result == 'no':
+        return
+    else:
+        for index in state["selected"]:
+
+            del students[index]
+
+            state["curr_page"] = 1  # if page is out of range
+
+            tableIntoCSVFile("students.csv")
+            getCSVTable("students.csv")
+
+            reloadTable()
+
+
 def createDialog(dialog_name):
     if dialog_name == "Search":
         renderSearchDialog()
@@ -1150,6 +1187,9 @@ def createDialog(dialog_name):
 
     if dialog_name == "Modify":
         renderNewDialog("modify")  # Reuse renderNewDialog function
+
+    if dialog_name == "Delete":
+        renderDeleteMsg()
 
     state["menu_open"] = None  # ---- Reset state so listener does not re-paint
 
@@ -1177,7 +1217,10 @@ window.resizable(False, False)
 # ------------------------------------------------------------------------------
 
 
-sidebar = tk.Frame(window, bg=PRIMARY_BG, height=WIN_HEIGHT, width=150)
+sidebar = tk.Frame(
+    window,
+    bg=PRIMARY_BG, height=WIN_HEIGHT,
+    width=150, name="sidebar")
 sidebar.grid(row=0, column=0)
 sidebar.grid_propagate(False)
 sidebar.grid_columnconfigure(0, weight=1)
@@ -1201,10 +1244,191 @@ sidebar.grid_rowconfigure(len(BUTTONS), weight=1)
 
 # -------------------------------- Data Display --------------------------------
 
+display = tk.Frame(
+    window, name="display",
+    height=WIN_HEIGHT,
+    width=1050, background=PRIMARY_BG)
+display.grid(row=0, column=1)
+display.grid_propagate(False)
+
+# ---------------------------------- Caption -----------------------------------
+
+caption = tk.Frame(
+    display,
+    height=20, width=1050,
+    background=PRIMARY_BG, name="caption")
+caption.grid(row=0, column=0)
+caption.grid_propagate(False)
+
+table_container = tk.Frame(
+    display, name="table_container",
+    height=400, width=1050,
+    background=ROW_BG_DARK)
+
+
+# -------------------------------- Table Creation ------------------------------
+# -                   Student table has its own create function.               -
+# -       If student data is modified, table is destroyed and recreated        -
+# ------------------------------------------------------------------------------
+
+def createTable(table_container):
+    cell_width = [35, 35, 20, 20, 20, 35, 7]
+
+    for index, c_name in enumerate(CAPTION_NAMES):
+        cap = tk.Label(
+            caption,
+            width=cell_width[index],
+            text=c_name,
+            anchor=tk.W,
+            background=PRIMARY_BG,
+            foreground=PRIMARY_FG)
+        cap.grid(row=0, column=index)
+        cap.grid_propagate(False)
+        caption.grid_columnconfigure(index, weight=1)
+
+# -------------------------------- Student List --------------------------------
+
+    table_container.grid(row=1, column=0)
+    table_container.grid_propagate(False)
+    table_container.grid_columnconfigure(0, weight=0)
+    table_container.grid_rowconfigure(0, weight=0)
+
+    if len(students):
+        # divide list into chunks of 20 items
+        chunks = [students[i:i + 20] for i in range(0, len(students), 20)]
+        state["total_pages"] = math.ceil(len(students) / 20)
+
+        # display current chunk of data
+        list_to_display = chunks[state["curr_page"] - 1]
+    else:
+        list_to_display = []
+
+    if len(list_to_display):
+        for row_index, student in enumerate(list_to_display):
+
+            # Every secound row darker
+            background = ROW_BG_LIGHT
+            foreground = "white"
+
+            if row_index % 2 == 0:
+                background = ROW_BG_DARK
+
+            # Check if student is selected
+            tab_index = (state["curr_page"] - 1) * 20 + row_index
+
+            selected = True if tab_index in state["selected"] else False
+
+            if selected:
+                background = SELECTED_BG
+                foreground = PRIMARY_BG
+
+            row = tk.Frame(
+                table_container, name=f"row_{tab_index}",
+                width=1050, height=20,
+                background=background)
+
+            student_row = [student["f_name"], student["l_name"], student["phone"],
+                           student["subjects"], student["dob"], student["email"], "X"]
+
+            # Show Student Information Details Function
+
+            def showStudentInfoDetails(index):
+                student = students[index]
+                f_name = "First Name    : " + student["f_name"]
+                l_name = "Last Name     : " + student["l_name"]
+                dob = "Date of Birth : " + student["dob"]
+                phone = "Phone Number  : " + student["phone"]
+                email = "Email         : " + student["email"]
+                sub_s = student["subjects"]
+
+                # split subject strings by 2-s
+                subs_abbr = [sub_s[i: i + 2] for i in range(0, len(sub_s), 2)]
+
+                # Assign string from abbriviation
+                decor = "\n" + (" " * 16) + u"\u2022 "
+                subs = list(map(lambda s: decor + SUBJECTS[s], subs_abbr))
+                sub_text = "".join(subs)
+
+                details = f"{f_name}\n{l_name}\n{dob}\n{phone}\n{email}\nSubjects      :{sub_text}"
+                messagebox.showinfo("Student Details", details)
+
+            # Iterate cells
+            for index, entry in enumerate(student_row):
+                # Student record
+                if index < 6:
+                    if index == 3:
+                        subjects = [entry[i: i + 2]
+                                    for i in range(0, len(entry), 2)]
+                        entry = subjects
+
+                    cell = tk.Label(
+                        row,
+                        width=cell_width[index],
+                        background=background,
+                        foreground=foreground,
+                        text=entry,
+                        font=MONOSPACE,
+                        anchor=tk.W)
+                    cell.bind("<Button 1>", lambda event,
+                              ind=tab_index: showStudentInfoDetails(ind))
+                else:
+                    # Toggle Selection
+                    def toggleSelect(index):
+                        updated_selected = state["selected"]
+                        if index in state["selected"]:
+                            updated_selected.remove(index)
+                        else:
+                            updated_selected.append(index)
+
+                        state["update"].append("table")
+                        state["update"].append("footer")
+
+                    # Selection Checkbox
+                    checkbox_text = u"\u25A3" if selected else u"\u25A1"
+                    checkbox_color = SELECTED_BG if selected else PRIMARY_FG
+
+                    cell = tk.Label(
+                        row,
+                        text=checkbox_text,
+                        foreground=checkbox_color,
+                        width=cell_width[index],
+                        background=PRIMARY_BG)
+
+                    # Bind click event to label using closures
+                    cell.bind("<Button-1>",
+                              lambda event, ind=tab_index: toggleSelect(ind))
+
+                cell.grid(row=0, column=index)
+                cell.grid_propagate(False)
+                row.grid_columnconfigure(index, weight=1)
+
+            row.grid_columnconfigure(index, weight=1)
+            row.grid(row=row_index, column=0)
+            row.grid_propagate(False)
+            table_container.grid_columnconfigure(index, weight=1)
+    else:
+        no_item = tk.Label(
+            table_container,
+            text="No Item Found!",
+            background=ROW_BG_DARK,
+            foreground=PRIMARY_FG,
+            width=150,
+            height=30)
+        no_item.grid(row=0, column=0)
+        no_item.grid_propagate(False)
+        table_container.grid_columnconfigure(0, weight=1)
+        table_container.grid_rowconfigure(0, weight=1)
+
+
+createTable(table_container)
+
 
 # --------------------------------- Footer -------------------------------------
 footer_container = tk.Frame(
-    display, height=32, width=1050, background=PRIMARY_BG)
+    display,
+    name="footer_container",
+    height=32, width=1050,
+    background=PRIMARY_BG)
 
 
 def createFooter(footer_container):
@@ -1296,7 +1520,6 @@ def createFooter(footer_container):
 
 
 # ----------------------------- Pagination Info ---------------------------------
-
 
     def paginate(direction):
         if direction == "+" and state["curr_page"] < state["total_pages"]:
